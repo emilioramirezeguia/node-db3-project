05334c04631ee021bb8faa6f2dfff0bf214bd5e3
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-12 13:31:37 -0700

    Add findSteps() database helper method

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -20,8 +20,17 @@ function findById(id) {
   return db("schemes").where({ id }).first();
 }
 
-function findSteps() {
-  return;
+function findSteps(id) {
+  return db("steps")
+    .join("schemes", "steps.scheme_id", "=", "schemes.id")
+    .select(
+      "steps.id",
+      "schemes.scheme_name",
+      "steps.step_number",
+      "steps.instructions"
+    )
+    .where({ "schemes.id": id })
+    .orderBy("steps.step_number");
 }
 
 function add() {
